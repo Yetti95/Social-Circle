c4ebd1d9aa124d8b9b64feb68d3431f14df6af26
isaacanielson <isaacanielson@gmail.com>
2024-01-25 20:02:05 -0700

    Pause Menu stable
    button functions not active

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,11 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _on_resume_pressed():
 	pass
+
+
+func _on_quit_pressed():
+	pass # Replace with function body.
